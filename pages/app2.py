6370969e340c05c27a41f034cafd27050e24c849
pmmc92 <pmmc92@users.noreply.github.com>
2022-08-25 15:02:09 +0100

    new page

---
+++ pages/app2.py
@@ -0,0 +1,5 @@
+
+st.header(":page_facing_up:" "Log File and structure")
+            st.subheader("Basic Information")
+            st.write("There are **{} patients** in this event log".format(log["case:concept:name"].nunique()))
+            st.dataframe(log)
