7582797f54f009962f6d796aa03a23e75b4726bd
pmmc92 <pmmc92@users.noreply.github.com>
2022-08-25 15:03:29 +0100

    update

--- pages/app2.py
+++ pages/app2.py
@@ -1,5 +1,5 @@
 
 st.header(":page_facing_up:" "Log File and structure")
-            st.subheader("Basic Information")
-            st.write("There are **{} patients** in this event log".format(log["case:concept:name"].nunique()))
-            st.dataframe(log)
+    st.subheader("Basic Information")
+    st.write("There are **{} patients** in this event log".format(log["case:concept:name"].nunique()))
+    st.dataframe(log)
